d6cd861e8e337cf8f7e6e6d0d3db3a43566e1023
JavokhirJava <abdinabiyev2001@gmail.com>
2023-03-13 14:30:56 +0500

    1-config and spring jdbc

--- src/main/java/org/example/config/Config.java
+++ src/main/java/org/example/config/Config.java
@@ -4,10 +4,29 @@ import org.example.dataBase.InitDataBase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class Config {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/library");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("Java2001");
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate getTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
 //    @Bean
 //    public InitDataBase initDataBase(){
 //        InitDataBase initDataBase = new InitDataBase();
